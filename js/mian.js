fd613e03fa6c91c8fcdb36ade09e8757add99793
Alexana7 <ngayane@bk.ru>
2023-12-14 16:54:51 +0300

    add swiperjs to slider

--- js/mian.js
+++ js/mian.js
@@ -23,3 +23,37 @@ document.addEventListener('click', () => {
 for(let hint of infoHints) {
   hint.addEventListener('click', (e)=> e.stopPropagation());
 }
+
+// swiper slider
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  slidesPerView: 4,
+  spaceBetween: 42,
+  freeMode: true,
+
+  // breakpoints: {
+  //   640: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   768: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 40,
+  //   },
+  //   1024: {
+  //     slidesPerView: 5,
+  //     spaceBetween: 50,
+  //   },
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#sliderNext',
+    prevEl: '#sliderPrev',
+  },
+
+});
